6cb44e85dca9d207115c44faaa3340eed69ec9e3
Richagogde03 <richagogde03@gmail.com>
2025-07-23 15:56:56 +0530

    Initial commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>
+            © 2025 EduSphere. All rights reserved.
+        </p>
+        <ul>
+            <li>Terms of Services</li>
+            <li>Privacy Policy</li>
+            <li>Terms of Services</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Footer
